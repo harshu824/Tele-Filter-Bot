d3a9e4c3fe1c8d6b184d6912a94c15f9e5dea8d5
harshu824 <harshitpandhit8953@gmail.com>
2025-09-15 06:55:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -40,7 +40,7 @@ VRFY_IMG = environ.get("VRFY_IMG", "https://envs.sh/R3N.jpg")
 
 # ─── Admins / Users / Channels ──────────────────────────
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5613512299').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '@Moviezzzzzz_only').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002440330128').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '0').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '').split()]
